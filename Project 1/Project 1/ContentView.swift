46459d99321c509575ee2234cc29a32966a8d2dd
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 09:49:30 +0700

    Reading text from the user with TextField

--- Project 1/Project 1/ContentView.swift
+++ Project 1/Project 1/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     @State var tapCount = 0
+    
     @State private var name = ""
     
     let students = ["Harry", "Hermione", "Ron"]
     @State private var selectedStudent = "Harry"
+    
+//    @State private var checkAmount = 0.0
+//    @State private var numberOfPeople = 2
+//    @State private var tipPercentage = 20
+//    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         
@@ -23,7 +29,7 @@ struct ContentView: View {
                 Section {
                     Text("Hello, world!")
                 }
-                
+
                 Section {
                     Text("Hello, world!")
                     Text("Hello, world!")
@@ -52,6 +58,17 @@ struct ContentView: View {
                 }
             }
         }
+        
+//        Form {
+//            Section {
+//                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+//                    .keyboardType(.decimalPad)
+//            }
+//
+//            Section {
+//                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+//            }
+//        }
         
     }
 }
