ac2fe30af68991f48d90493b51263901b284e2b9
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 17:00:22 +0700

    Adding a segmented control for tip percentages

--- Project 1/Project 1/ContentView.swift
+++ Project 1/Project 1/ContentView.swift
@@ -18,8 +18,8 @@ struct ContentView: View {
     
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
-    let tipPercentages = [10, 15, 20, 25, 0]
+    @State private var tipPercentage = 10
+    let tipPercentages = [0, 10, 15, 20, 25]
     
     var body: some View {
         
@@ -79,6 +79,18 @@ struct ContentView: View {
                             }
                         }
                     }
+                    
+                    Section{
+                        Picker("Tip Percentage", selection: $tipPercentage){
+                            ForEach(tipPercentages, id: \.self){
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How much tip do you want to leave?")
+                    }
+                    
                     Section {
                         Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                     }
