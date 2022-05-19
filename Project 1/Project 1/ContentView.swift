3bec3570c950c16726afdcf69f56ecde0091337f
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 09:33:42 +0700

    Update ContentView.swift

--- Project 1/Project 1/ContentView.swift
+++ Project 1/Project 1/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tapCount = 0
+    @State private var name = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            // Creating a form
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+                
+                Section {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            // Navigation title size
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
+        }
+        
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Hello, world!")
+        }
+        
     }
 }
 
