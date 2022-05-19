6b7cbc11545c3683e26af2d191469d8ae7d2ac63
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 17:13:10 +0700

    project 2 init

---
+++ Project 2/Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by MacBook Pro on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
