d43356fc14771ccaf0b17e2ac57116476d7aeb82
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 17:32:13 +0700

    Gradients

--- Project 2/Project 2/ContentView.swift
+++ Project 2/Project 2/ContentView.swift
@@ -9,18 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-
-            Text("Your content")
-                .foregroundColor(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        //        ZStack {
+        //            VStack(spacing: 0) {
+        //                Color.red
+        //                Color.blue
+        //            }
+        //
+        //            Text("Your content")
+        //                .foregroundColor(.secondary)
+        //                .padding(50)
+        //                .background(.ultraThinMaterial)
+        //        }
+        //        .ignoresSafeArea()
+        
+        LinearGradient(gradient: Gradient(stops: [
+            .init(color: .white, location: 0.45),
+            .init(color: .black, location: 0.55),
+        ]), startPoint: .top, endPoint: .bottom)
+        
+        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        
+        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
     }
 }
 
