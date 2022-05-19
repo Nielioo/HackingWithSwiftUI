7041cdc310ccf534c4c5988d0dbc496905f77f28
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 09:52:53 +0700

    Update ContentView.swift

--- Project 1/Project 1/ContentView.swift
+++ Project 1/Project 1/ContentView.swift
@@ -16,11 +16,6 @@ struct ContentView: View {
     let students = ["Harry", "Hermione", "Ron"]
     @State private var selectedStudent = "Harry"
     
-//    @State private var checkAmount = 0.0
-//    @State private var numberOfPeople = 2
-//    @State private var tipPercentage = 20
-//    let tipPercentages = [10, 15, 20, 25, 0]
-    
     var body: some View {
         
         NavigationView{
@@ -58,17 +53,6 @@ struct ContentView: View {
                 }
             }
         }
-        
-//        Form {
-//            Section {
-//                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-//                    .keyboardType(.decimalPad)
-//            }
-//
-//            Section {
-//                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-//            }
-//        }
         
     }
 }
